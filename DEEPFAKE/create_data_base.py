f7b0e5854a8c36570dc9a4d5735b1602f0a59e69
rkycrwlr <rkycrwlr@proton.me>
2023-06-20 16:33:47 +0200

    create_database I

---
+++ DEEPFAKE/create_data_base.py
@@ -0,0 +1,29 @@
+
+from duckduckgo_search import ddg_images
+from fastcore.all import *
+from fastai.vision.all import *
+
+def search_images(term, max_images=30):
+    print(f"Searching for '{term}'")
+    return L(ddg_images(term, max_results=max_images)).itemgot('image')
+
+dest = Path("D_Trump")
+
+dest.mkdir(exist_ok=True, parents=True)
+download_images(dest, urls=search_images("Donald Trump photo", max_images=100))
+download_images(dest, urls=search_images("Donald Trump smiling photo", max_images=100))
+download_images(dest, urls=search_images("Donald Trump speaking photo", max_images=100))
+download_images(dest, urls=search_images("Donald Trump raising hand photo", max_images=100))
+download_images(dest, urls=search_images("Donald Trump black and white photo", max_images=100))
+resize_images(dest, max_size=400, dest=dest)
+
+dest = Path("D_Trump")
+failed = verify_images(get_image_files(dest))
+failed.map(Path.unlink)
+
+
+
+
+
+
+# %%
